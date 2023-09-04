0f53e795c7ae5edf5e291d8d857fb02c05401b30
jjacob2003 <125762601+jjacob2003@users.noreply.github.com>
2023-09-04 18:29:44 -0500

    Add files via upload

---
+++ puzzle sets.py
@@ -0,0 +1,125 @@
+############################### ALl Puzzle Sets ###############################
+
+
+
+##### Puzzle 1 ################################################################
+# row 1
+print("(assert P_1_1_8)")
+print("(assert P_1_2_2)")
+print("(assert P_1_5_9)")
+# row 2
+print("(assert P_2_4_6)")
+print("(assert P_2_5_2)")
+# row 3
+print("(assert P_3_1_3)")
+print("(assert P_3_8_8)")
+# row 4
+print("(assert P_4_4_3)")
+print("(assert P_4_6_7)")
+print("(assert P_4_9_4)")
+# row 5
+print("(assert P_5_1_4)")
+print("(assert P_5_9_1)")
+# row 6
+print("(assert P_6_3_3)")
+# row 7
+print("(assert P_7_1_9)")
+print("(assert P_7_8_7)")
+# row 8
+print("(assert P_8_3_1)")
+print("(assert P_8_4_9)")
+print("(assert P_8_5_5)")
+print("(assert P_8_8_4)")
+print("(assert P_8_9_8)")
+# row 9
+print("(assert P_9_3_6)")
+print("(assert P_9_5_1)")
+print("(assert P_9_9_5)")
+###############################################################################
+
+
+
+###### Puzzle 2 ###############################################################
+#row1
+print("(assert P_1_6_3)")
+print("(assert P_1_8_8)")
+#row2
+print("(assert P_2_3_6)")
+print("(assert P_2_5_7)")
+print("(assert P_2_8_5)")
+#row3
+print("(assert P_3_2_1)")
+print("(assert P_3_5_2)")
+print("(assert P_3_7_9)")
+#row4
+print("(assert P_4_9_1)")
+#row5
+print("(assert P_5_1_5)")
+print("(assert P_5_2_2)")
+#row6
+print("(assert P_6_1_7)")
+print("(assert P_6_3_9)")
+print("(assert P_6_4_4)")
+print("(assert P_6_6_6)")
+print("(assert P_6_7_3)")
+#row7
+print("(assert P_7_4_2)")
+print("(assert P_7_9_7)")
+#row8
+print("(assert P_8_3_5)")
+print("(assert P_8_5_4)")
+#row9
+print("(assert P_9_1_3)")
+print("(assert P_9_4_8)")
+###############################################################################
+
+
+
+###### Puzzle 3 ###############################################################
+#row1
+valid.write("(assert P_1_1_2)")
+valid.write("(assert P_1_3_1)")
+valid.write("(assert P_1_8_4)")
+#row2
+valid.write("(assert P_2_4_3)")
+valid.write("(assert P_2_5_2)")
+#row3
+valid.write("(assert P_3_5_5)")
+valid.write("(assert P_3_8_7)")
+valid.write("(assert P_3_9_6)")
+#row4
+valid.write("(assert P_4_6_8)")
+#row5
+valid.write("(assert P_5_1_4)")
+valid.write("(assert P_5_2_5)")
+valid.write("(assert P_5_6_7)")
+#row6
+valid.write("(assert P_6_3_8)")
+valid.write("(assert P_6_5_1)")
+valid.write("(assert P_6_6_3)")
+valid.write("(assert P_6_9_7)")
+#row7
+valid.write("(assert P_7_7_8)")
+valid.write("(assert P_7_9_5)")
+#row8
+valid.write("(assert P_8_1_6)")
+valid.write("(assert P_8_2_8)")
+#row9
+valid.write("(assert P_9_3_3)")
+valid.write("(assert P_9_4_6)")
+valid.write("(assert P_9_9_1)")
+###############################################################################
+
+
+
+###### Puzzle 4 ###############################################################
+#row1
+#row2
+#row3
+#row4
+#row5
+#row6
+#row7
+#row8
+#row9
+###############################################################################
